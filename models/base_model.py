a29d238fec93b817cc4afccfbf4069bb70012816
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-19 20:26:38 +0100

    Update the init method to include a fallback in case there is no kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -29,10 +29,10 @@ class BaseModel:
                 self.created_at = datetime.now()
             if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
-        # else:
-        #     self.id = str(uuid.uuid4())
-        #     self.created_at = datetime.now()
-        #     self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
