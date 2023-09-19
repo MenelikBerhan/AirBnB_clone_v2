91b4bf72552a626f0749bed207ed5817f2cc90b3
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-09-19 04:05:07 +0300

    Refactor and edit:
    Change import method by using import_module in console
    Remove storage.save() in console.do_create. Seems to work.
    Use environ.get() to access env variables. Used indexing before.
    Add ondelete=cascade for Foreign key columns.

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
@@ -17,7 +17,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            # from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -31,7 +30,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        cls = self.__class__.__name__
         obj_dict = self.__dict__.copy()
         if '_sa_instance_state' in obj_dict:
             del obj_dict['_sa_instance_state']
@@ -48,8 +47,7 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary.update({'__class__': self.__class__.__name__})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary:
