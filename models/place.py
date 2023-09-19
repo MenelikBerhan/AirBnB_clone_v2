3fe858b323a9ebc23b083ee39d145636c09c16b9
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-19 20:03:18 +0100

    Move the place_amenity table to the top of the file

--- models/place.py
+++ models/place.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """ Place Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -22,22 +29,33 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    reviews = relationship('Review', backref='place',
-                           cascade='all, delete-orphan')
+    # if getenv("HBNB_TYPE_STORAGE") == "db":
+    #     reviews = relationship('Review', backref='place',
+    #                            cascade='all, delete-orphan')
+    #     amenities = relationship('Amenity', secondary=place_amenity,
+    #                              back_populates='place_amenities', viewonly=False)
+    # else:
+    #     @property
+    #     def reviews(self):
+    #         """returns the list of `Review` instances with `place_id` equals
+    #         to the current Place.id"""
+    #         from models import storage
+    #         from models.review import Review
+    #         return [review for review in storage.all(Review).values()
+    #                 if review.place_id == self.id]
+    #     # return [review for review in self.reviews if review.place_id == self.id]
 
-    @property
-    def reviews(self):
-        """returns the list of `Review` instances with `place_id` equals
-        to the current Place.id"""
-        from models import storage
-        from models.review import Review
-        return [review for review in storage.all(Review).values()
-                if review.place_id == self.id]
-    # return [review for review in self.reviews if review.place_id == self.id]
+    #     @property
+    #     def amenities(self):
+    #         """returns the list of 'Amenity' instances based on the attribute"""
+    #         from models import storage
+    #         from models.amenity import Amenity
+    #         return [amenity for amenity in storage.all(Amenity).values()
+    #                 if amenity.id in self.amenity_ids]
 
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
+    # @amenities.setter
+    # def amenities(self, amenity=None):
+    #     from models.amenity import Amenity
+    #     """Appends amenity ids to the attribute"""
+    #     if type(amenity) is Amenity and amenity.id not in self.amenity_ids:
+    #         self.amenity_ids.append(amenity.id)
