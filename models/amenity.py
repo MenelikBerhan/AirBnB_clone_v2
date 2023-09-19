20869e8d87ebafe58c08f46e83131c64aa412585
Toby2507 <oluwatobisalau2000@gmail.com>
2023-09-19 19:05:34 +0100

    Add the place_amenities class attribute that represents a many-to-many relationship between the place and amenity class to the amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """ State Module for HBNB project """
+from os import getenv
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
     """Serves as the blueprint for future amenities"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place", secondary=place_amenity, back_populates="amenities")
