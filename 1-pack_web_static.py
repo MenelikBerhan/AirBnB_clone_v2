846e5ea4ad4627bec4634772a25e7ccb2d3d9444
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-05 23:32:02 +0300

    Task 3 and use utc time zone for archive name

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,7 @@ import os
 
 def do_pack():
     """ generates a .tgz archive from the contents of the web_static folder"""
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     name = "versions/web_static_{}.tgz".format(now)
     if not os.path.isdir("versions"):
         local("mkdir versions")
