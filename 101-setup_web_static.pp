2f7bb66bfb6791b86296c4dc90a3115d519c8720
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-08 18:45:36 +0300

    Use file to configure nginx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,4 +1,39 @@
 # Set up my webservers for web server use
+$config_text="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	location /hbnb_static {
+		alias /data/web_static/current/;
+	}
+
+	add_header X-Served-By \$hostname;	
+
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}
+
+	error_page 404 /404.html;
+	location /404.html {
+		internal;
+	}
+}"
+$test_content="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>\n"
+$err_content="Ceci n'est pas une page\n"
+$default_content="Holberton School\n"
 
 exec { 'update_and_install_nginx':
   command  => 'apt-get update; apt-get -y install nginx',
@@ -8,16 +43,24 @@ exec { 'update_and_install_nginx':
 command  => 'mkdir -p /data/web_static/shared; mkdir -p /data/web_static/releases/test; ',
 provider => shell
 }
--> exec {'create_test_index':
-command  => 'echo "Menelik Berhan" > /data/web_static/releases/test/index.html',
-provider => shell
+-> file {'create_test_index':
+path    => '/data/web_static/releases/test/index.html',
+content => $test_content
+}
+-> file {'nginx_config':
+  path    =>  '/etc/nginx/sites-available/default',
+  content => $config_text
+}
+-> file {'index_html':
+  path    =>  '/var/www/html/index.html',
+  content => $default_content
+}
+-> file {'404_html':
+  path    =>  '/var/www/html/404.html',
+  content => $err_content
 }
 -> exec {'symbolic_link_change_owner':
 command  => 'ln -sf /data/web_static/releases/test /data/web_static/current; chown -R ubuntu:ubuntu /data/;',
-provider => shell
-}
--> exec {'config_nginx':
-command  => 'sudo sed -i "s/server_name _;/server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}|" /etc/nginx/sites-available/default',
 provider => shell
 }
 -> exec {'restart_nginx':
