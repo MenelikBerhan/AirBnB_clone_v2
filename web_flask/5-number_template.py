4ae6c76936912d1cbb75c71399919c276f04c853
Menelik_Berhan <MenelikBerhan@gmail.com>
2023-10-21 01:29:20 +0300

    Task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 app = Flask(__name__)
 
@@ -34,6 +34,12 @@ def pythonText(text='is cool'):
 def number(n):
     """Displays 'n is a number' if n is an integer"""
     return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display a HTML page only if n is an integer"""
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
